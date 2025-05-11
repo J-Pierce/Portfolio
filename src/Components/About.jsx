a34e03443c5c314d64a98304953b97ca343c43aa
J-Pierce <james.pierce@hotmail.co.uk>
2025-05-11 17:30:58 +0100

    about and project progress

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,31 @@
-import image from "../assets/images.png";
+import profile from "../assets/Me.png";
 
 export default function About() {
   return (
-    <section id="About">
+    <section className="about">
       <h2>About</h2>
-      <img src={image} alt="placeholder image" />
-      <p>hello</p>
+      <section className="row">
+        <img src={profile} alt="placeholder image" id="profileImage" />
+        <section className="profileText">
+          <section className="name">
+            <p id="name">James Pierce</p>
+            <p id="pronouns">He/Him</p>
+          </section>
+          <p id="location">Essex, United Kingdom</p>
+          <p id="statement">
+            Academic applications of modelling and coding experienced during my
+            degree, and PhD, is what sparked my interest in software
+            development. Being someone who loves challenges and problem solving,
+            I am greatly interested in pursuing a career that allows me to use
+            these skills on a day-to-day basis. With my time at Northcoders I
+            have gained valuable experience in both front end and back end
+            development and am keen to develop these skills further. I am a
+            motivated and dependable individual and, alongside my time in both
+            industry and academia in which I developed strong communication and
+            team working skills, I believe I would be an asset to any team.
+          </p>
+        </section>
+      </section>
     </section>
   );
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,10 +1,31 @@
 import NCnews from "../assets/NC-News.png";
+import Backend from "../assets/Backend.png";
+import pokemonBattler from "../assets/pokemonBattler.png";
 
 export default function Projects() {
   return (
-    <section id="Projects">
+    <section className="projects">
       <h2>Projects</h2>
-      <img src={NCnews} alt="NC News project" />
+      <section className="projectsGrid">
+        <a href={"https://github.com/J-Pierce/NC-News-FE"}>
+          <section className="project">
+            <h3>NC News Frontend</h3>
+            <img src={NCnews} alt="NC News Frontend project" />
+          </section>
+        </a>
+        <section className="project">
+          <a href={"https://github.com/J-Pierce/NC-News-BE"}>
+            <h3>NC News Backend</h3>
+            <img src={Backend} alt="NC News Backend project" />
+          </a>
+        </section>
+        <section className="project">
+          <a href={"https://github.com/J-Pierce/Pokemon_Battler"}>
+            <h3>Pokemon Battler</h3>
+            <img src={pokemonBattler} alt="NC News project" />
+          </a>
+        </section>
+      </section>
     </section>
   );
 }
