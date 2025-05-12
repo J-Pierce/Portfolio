d5ddc14c75064af95282fcf15c83e8296472266c
J-Pierce <james.pierce@hotmail.co.uk>
2025-05-13 00:19:47 +0100

    about section restyling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,46 +1,66 @@
 import profile from "../assets/profile.png";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function About() {
   return (
     <section className="about">
-      <h2>About</h2>
-      <section className="row">
-        <img src={profile} alt="placeholder image" id="profileImage" />
-        <section className="profileText">
-          <section className="name">
-            <p id="name">James Pierce</p>
-            <p id="pronouns">He/Him</p>
-          </section>
+      <section className="grid">
+        <div>
+          <img src={profile} alt="placeholder image" id="profileImage" />
+        </div>
+
+        <div className="text">
+          <p id="name">James Pierce</p>
           <p id="role">Junior Software Developer</p>
-          <p id="location">Essex, United Kingdom</p>
-        </section>
-        <section className="connect">
-          <p id="title">Connect with me</p>
-          <p>
-            <a href="mailto:contact@jamespierce.co.uk">
-              contact@jamespierce.co.uk
-            </a>
-          </p>
-          <p>
-            <a href="contact@jamespierce.co.uk">LinkedIn</a>
-          </p>
-          <p>
-            <a href="contact@jamespierce.co.uk">Github</a>
-          </p>
-        </section>
+          <section id="statement">
+            <p>
+              Academic applications of modelling and coding experienced during
+              my degree is what sparked my interest in software development.
+              Being someone who loves challenges and problem solving, I am
+              greatly interested in pursuing a career that allows me to use
+              these skills on a day-to-day basis.
+            </p>
+            <p>
+              With my time at Northcoders I have gained valuable experience in
+              both front end and back end development and am keen to develop
+              these skills further. I am a motivated and dependable individual
+              and, alongside my time in both industry and academia in which I
+              developed strong communication and team working skills, I believe
+              I would be an asset to any team.
+            </p>
+          </section>
+          <section className="connect">
+            <p id="title">Connect with me:</p>
+            <ul className="links">
+              <li>
+                <a href="mailto:contact@jamespierce.co.uk">
+                  <section className="link">
+                    <MailOutlineIcon /> <p>Email</p>
+                  </section>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/james-t-pierce/">
+                  <section className="link">
+                    <LinkedInIcon className="icon" />
+                    <p>LinkedIn</p>
+                  </section>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/J-Pierce/">
+                  <section className="link">
+                    <GitHubIcon />
+                    <p>Github</p>
+                  </section>
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
       </section>
-      <p id="statement">
-        Academic applications of modelling and coding experienced during my
-        degree, and PhD, is what sparked my interest in software development.
-        Being someone who loves challenges and problem solving, I am greatly
-        interested in pursuing a career that allows me to use these skills on a
-        day-to-day basis. With my time at Northcoders I have gained valuable
-        experience in both front end and back end development and am keen to
-        develop these skills further. I am a motivated and dependable individual
-        and, alongside my time in both industry and academia in which I
-        developed strong communication and team working skills, I believe I
-        would be an asset to any team.
-      </p>
     </section>
   );
 }
